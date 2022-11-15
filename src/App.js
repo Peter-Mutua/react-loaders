f067b3a10de2a78ddbc8566eb44c9836ea0cf2a0
Peter-Mutua <petermwendwa94@gmail.com>
2022-11-15 16:35:03 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import { BallTriangle } from 'react-loader-spinner';
+
 function App() {
     const [post, setPost] = useState(null);
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+        fetch(`https://jsonplaceholder.typicode.com/posts`)
         .then((res) => res.json())
             .then((data) => {
                 const { body } = data;
@@ -20,7 +22,18 @@ function App() {
         {/*!!We add a carnary statement to show loading
         * if (post == null { Loading... } { Post }
         */}
-        { post ? post : 'Loading...' }
+        { post ? post : 
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperClass= "loader"
+            wrapperStyle=""
+            visible={true}
+        />
+         }
     </div>
   );
 }
